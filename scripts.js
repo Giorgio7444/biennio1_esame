6de6c4f5ab9b77e6c52ca3588dd86a36f511d82a
Giorgio Pulli <giorgiopulli@gmail.com>
2025-02-01 22:28:09 +0100

    html&js: added swiper and favicon - css: added tertiary color, adjusted accordion and swiper

--- scripts.js
+++ scripts.js
@@ -80,4 +80,20 @@ document.addEventListener("DOMContentLoaded", () => {
   setupTitle();
   setupAccordion();
   setupMenu();
+  
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
+
 });
